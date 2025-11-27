5549d8513cb66c5f429993dc4ae290a9b22dbcac
Jerry Lam <159370584+JerryLam1@users.noreply.github.com>
2025-11-26 21:12:27 -0800

    Add files via upload

---
+++ wordle-main.asm
@@ -0,0 +1,110 @@
+#Jerry Lam, Brandon Ho, Christopher Tjahja, Duve Rodriguez-Garcia
+#pseudorandom randomizer for picking what word is chosen
+.include "commonMacros.asm"
+.include "wordList.asm"
+
+.data
+guessBuffer: .space 16 #enough for 16 bytes - 5 letters, newline, null terminator, and 9 extra
+guessPrompt: .asciiz "\nPlease enter a 5-letter guess: "
+invalidMsg: .asciiz "\nInvalid input. Please enter exactly 5 letters (A-Z)."
+
+.text
+main:
+	jal getRandomWord
+	jal getGuess
+	
+	#print guessBuffer for testing purposes - REMOVE IN SUBMISSION
+	li $v0, 4
+	la $a0, guessBuffer
+	syscall
+	exit
+
+getRandomWord:
+	li $v0, 30	#get system time and set $t0 to the value
+	syscall
+	move $t0, $a0
+	
+	li $t1, 60	#60 words, so the index should be time % 60
+	divu $t0, $t1	#HI register is set to the remainder (0-59)
+	mfhi $t2	#t2 = (0-59) random
+	
+	#load pointer to random word using value in $t2
+	sll $t3, $t2, 2		#multiply $t2 by 4 and store in $t3 since each word is stored as 4 bytes - $t3 = offset from index 0
+	
+	la $s0, wordList
+	addu $s0, $s0, $t3	#set $s0 pointer to 0 + offset in $t3
+	lw $s1, 0($s0)	#chosen word is saved in $s1
+	
+	#print the chosen word for testing - REMOVE FOLLOWING LINES IN SUBMISSION
+	move $a0, $s1
+	li $v0, 4
+	syscall
+	
+	jr $ra
+
+#getGuess - reads user guess into guessBuffer
+#ensures that length = 5, no characters/numbers, converts lowercase into uppercase
+getGuess:
+	printString(guessPrompt)	#print the prompt
+	#read into buffer
+	readString(guessBuffer, 16) #16 chars maximum
+	
+	la $s2, guessBuffer	#load address of guessBuffer into $s2
+	li $t4, 0	#length counter
+
+stripLoop:	#loop to strip new line character to ensure proper input
+	lb $t5, 0($s2)	#load current char from input buffer
+	beqz $t5, endStripLoop	#null terminator ASCII = 0, so end loop when $t5 = 0
+	
+	#check for newline
+	li $t6, 10 #ASCII for \n = 10
+	beq $t5, $t6, stripNewLine
+	
+	addi $t4, $t4, 1	#advance counter and pointer
+	addi $s2, $s2, 1
+	j stripLoop
+
+stripNewLine:
+	sb $zero, 0($s2)	#once at the location of \n, change value to 0
+	j endStripLoop
+	
+endStripLoop:
+	li $t6, 5
+	bne $t4, $t6, invalidInput	#if the counter does not equal 5, invalid input
+	
+	la $s2, guessBuffer		#reset guessBuffer pointer to default
+
+validateLoop:	#validate input for no chars/nums, convert lowercase to uppercase
+	lb $t5, 0($s2)	#load current char from input buffer
+	beqz $t5, validInput	#if reach end of input w/o issues, then it is valid
+	
+	#check lowercase - if not, then check that the char is an uppercase letter
+	li $t6, 'a'
+	blt $t5, $t6, checkUppercase
+	
+	li $t7, 'z'
+	bgt $t5, $t7, checkUppercase
+	
+	addi $t5, $t5, -32	#to change characters from lowercase to uppercase, ASCII of uppercase is 32 less than lowercase
+	sb $t5, 0($s2)
+	j nextChar
+	
+checkUppercase:
+	#check for uppercase A-Z, same process
+	#if not uppercase A-Z, then invalid input
+	li $t6, 'A'
+	blt $t5, $t6, invalidInput
+	
+	li $t7, 'Z'
+	bgt $t5, $t7, invalidInput
+
+nextChar:
+	addi $s2, $s2, 1
+	j validateLoop
+
+validInput:	
+	jr $ra
+
+invalidInput:
+	printString(invalidMsg)
+	j getGuess
